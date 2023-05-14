43a376a9a4410e8352ec79c1b0ff72c6da93cfcd
Marcy <nadaso8th@gmail.com>
2023-05-14 12:19:39 -0500

    setup project file

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::path::Path;
+use std::io::prelude::*;
+use std::fs::File;
+
+ fn main() {
+    //collects commandline arguments into a vector
+    let args: Vec<String> = std::env::args().collect();
+
+    
+
+}
